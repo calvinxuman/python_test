ea7fedddf4af789f991689e84c7a76e920ea81dc
xuman <13207140256@139.com>
2017-11-30 15:37:47 +0800

    remove last print(num)

--- exercise_001.py
+++ exercise_001.py
@@ -1,8 +1,7 @@
 L1=list(range(1,6))
-num=0
+# num=0
 for i in L1:
     for j in L1:
         for k in L1:
             if i!=j and j!=k and i!=k:
                 print(i,j,k)
-                num += 1
