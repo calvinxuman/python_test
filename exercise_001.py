7ab73b87e1d85e221ea73d2a437eb82f07507d39
calvin <13207140256@163.com>
2017-11-30 15:17:26 +0800

    test

---
+++ exercise_001.py
@@ -0,0 +1,9 @@
+L1=list(range(1,6))
+num=0
+for i in L1:
+    for j in L1:
+        for k in L1:
+            if i!=j and j!=k and i!=k:
+                print(i,j,k)
+                num += 1
+print(num)
