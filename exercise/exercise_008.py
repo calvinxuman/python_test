4ea185d25e60a18d5b0ab519cf76fb04ce072c5a
xuman <13207140256@139.com>
2017-12-11 17:21:13 +0800

    train_tickets

--- exercise/exercise_008.py
+++ exercise/exercise_008.py
@@ -1,15 +1,8 @@
-def is_palindrome():
-    n=1
-    while True:
-        s=str(n)
-        s1=s[::-1]
-        if s==s1:
-            yield n
-        n+=1
+def is_palindrome(n):
+    s=str(n)
+    s1=s[::-1]
+    if s==s1:
+        return n
 
-j=20
-for i in is_palindrome():
-    print(i)
-    j -= 1
-    if j < 1 :
-        break
+list1=list(filter(is_palindrome,range(1,1000)))
+print(list1)
