a1db729e1bb9e7b28f5869c5bd60016821d4db9f
xuman <13207140256@139.com>
2017-12-19 22:49:04 +0800

    操作Excel

--- test_api/interface_test/login.py
+++ test_api/interface_test/login.py
@@ -18,31 +18,31 @@ class Login(object):
 
 
 class LoginTest (unittest.TestCase):
+    list_phone = []
+    list_password = []
+    list_equipment = []
+    dict_parameter = {'phone': list_phone, 'password': list_password, 'equipment': list_equipment}
     @classmethod
     def setUpClass(cls):
-        list_phone = []
-        list_password = []
-        list_equipment = []
-        dict_parameter = {'phone': list_phone, 'password': list_password, 'equipment': list_equipment}
         data = xlrd.open_workbook(
             r'C:\Users\Administrator\PycharmProjects\python_test\test_api\test_case\login_testcase.xlsx')
         table = data.sheet_by_name('login')
         for i in table.col_values(5, 3):
             if i != '':
-                list_phone.append(i)
+                LoginTest.list_phone.append(str(i))
         for j in table.col_values(6, 3):
             if j != '':
-                list_password.append(j)
+                LoginTest.list_password.append(str(j))
         for k in table.col_values(7, 3):
             if k != '':
-                list_equipment.append(k)
-        print(dict_parameter['phone'][2])
+                LoginTest.list_equipment.append(str(k))
+        print(LoginTest.dict_parameter)
 
     def test_login_001(self):
         # 未更换设备且用户名密码正确
-
-        self.login_001 = Login('13207140256',
-                               '8f7f8aca7230e2bbf851189f8da681e3443200fb', '5A9E2398DC25406E9A41F7B283E1A3FE')
+        self.login_001 = Login(LoginTest.dict_parameter['phone'][0],
+                               LoginTest.dict_parameter['password'][0],
+                               LoginTest.dict_parameter['equipment'][0])
         print('The Login_001 parameter is \n phone:' + self.login_001.phone + '\n password:' +
               self.login_001.password + '\n equipment:' + self.login_001.equipment)
         self.result_001 = self.login_001.login_test()
@@ -50,8 +50,9 @@ class LoginTest (unittest.TestCase):
 
     def test_login_002(self):
         # 未更换设备，密码错误
-        self.login_002 = Login('13207140256',
-                               '111111', '5A9E2398DC25406E9A41F7B283E1A3FE')
+        self.login_002 = Login(LoginTest.dict_parameter['phone'][1],
+                               LoginTest.dict_parameter['password'][1],
+                               LoginTest.dict_parameter['equipment'][1])
         print('The Login_002 parameter is \n phone:' + self.login_002.phone + '\n password:' +
                self.login_002.password + '\n equipment:' + self.login_002.equipment)
         self.result_002 = self.login_002.login_test()
@@ -59,8 +60,9 @@ class LoginTest (unittest.TestCase):
 
     def test_login_003(self):
         # 手机号未注册
-        self.login_003 = Login('13307140256',
-                               '222222', '5A9E2398DC25406E9A41F7B283E1A3FE')
+        self.login_003 = Login(LoginTest.dict_parameter['phone'][2],
+                               LoginTest.dict_parameter['password'][2],
+                               LoginTest.dict_parameter['equipment'][2])
         print('The Login_003 parameter is \n phone:' + self.login_003.phone + '\n password:' +
                self.login_003.password + '\n equipment:' + self.login_003.equipment)
         self.result_003 = self.login_003.login_test()
